3cd968b13dd3bd03430f2ea21a50aacf8d3ca39d
Casper Lisager Frandsen <casper_lf@yahoo.dk>
2023-12-15 16:31:59 +0100

    Uploaded day 13 and 14. Misunderstood for a while on day 14 what a cycle really meant.

---
+++ day13/fsharp/Program.fs
@@ -0,0 +1,94 @@
+﻿open System
+open System.IO
+
+type Stuff =
+    | Ashs
+    | Rock
+
+type Field = array<array<Stuff>>
+
+type MirrorLine = int * int
+
+
+let parseFile : string -> list<Field> =
+    File.ReadLines
+    >> Seq.toList
+    >> fun x -> List.foldBack (fun x (acc::accs) -> if x = "" then []::acc::accs else (x::acc)::accs) x [[]]
+    >> List.map (Array.ofList >> Array.map (Array.ofSeq >> Array.map (fun x -> if x = '#' then Rock else Ashs)))
+
+
+let isMirror (field : Field) ((leftI, rightI) : MirrorLine) : bool =
+    let left = field[..leftI] |> Array.rev
+    let right = field[rightI..]
+    let minL = min (Array.length left) (Array.length right) - 1
+    left[..minL] = right[..minL]
+
+let findMirror (field : Field) : bool * MirrorLine =
+    let pairsX = [for x in [0..Array.length field - 2] do yield (x,x+1)]
+    let pairsY = [for x in [0..Array.length field[0] - 2] do yield (x,x+1)]
+    let xResult = List.tryFind (isMirror field) pairsX
+    let yResult = List.tryFind (isMirror (Array.transpose field)) pairsY
+    match xResult, yResult with
+    | Some line, None -> (false, line)
+    | None, Some line -> (true, line)
+    | _ -> failwith "Could not find mirror point"
+
+let isSmudgedMirror (field : Field) ((leftI, rightI) : MirrorLine) : bool =
+    let left = field[..leftI] |> Array.rev
+    let right = field[rightI..]
+    let minL = min (Array.length left) (Array.length right) - 1
+    let equals (left : array<Stuff>) (right : array<Stuff>) : int =
+        //printfn "%A\n%A" left right
+        (left, right)
+        ||> Array.map2 (fun l r -> if l = r then 1 else 0)
+        |> Array.sum
+    //printfn "minl %A" minL
+    //printfn "%A\n\n%A" left[..minL] right[..minL]
+    (left[..minL], right[..minL])
+    ||> Array.map2 equals
+    //|> fun x -> printfn "%A" x; x
+    |> Array.fold (fun (acc, oneWrong) x ->
+                   if x = Array.length left[0] then (acc, oneWrong)
+                   elif oneWrong || x < Array.length left[0] - 1 then (false, oneWrong)
+                   else (acc, true)) (true,false)
+
+    //|> fun x -> printfn "%A" x; x
+    |> fun (x1, x2) -> x1 && x2
+    //|> fst
+
+let findSmudgedMirror (field : Field) : bool * MirrorLine =
+    let pairsX = [for x in [0..Array.length field - 2] do yield (x,x+1)]
+    let pairsY = [for x in [0..Array.length field[0] - 2] do yield (x,x+1)]
+    //printfn "%A" pairsX
+    //printfn "%A" pairsY
+    let xResult = List.tryFind (isSmudgedMirror field) pairsX
+    let yResult = List.tryFind (isSmudgedMirror (Array.transpose field)) pairsY
+    //printfn "%A" <| (xResult, yResult)
+    match xResult, yResult with
+    | Some line, None -> (false, line)
+    | None, Some line -> (true, line)
+    | Some line1, Some line2 ->
+        printfn "Found second mirror point %A" line2
+        (false, line1)
+    | _ -> failwith "Could not find mirror point"
+
+
+let partOne (fields : list<Field>) =
+    fields
+    |> List.map (findMirror >> fun (transpose, (x1,_)) -> if transpose then x1+1 else 100*(x1+1))
+    |> List.sum
+
+let partTwo (fields : list<Field>) =
+    fields
+    |> List.map (findSmudgedMirror >> fun (transpose, (x1,_)) -> if transpose then x1+1 else 100*(x1+1))
+    |> List.sum
+
+
+let fieldsSimple = parseFile "../input_simple.txt"
+let fields = parseFile "../input.txt"
+
+printfn "%A" <| partOne fieldsSimple
+printfn "%A" <| partOne fields
+
+printfn "%A" <| partTwo fieldsSimple
+printfn "%A" <| partTwo fields
---
+++ day14/fsharp/Program.fs
@@ -0,0 +1,112 @@
+﻿open System
+open System.IO
+
+type Cell =
+    | Round
+    | Cubed
+    | Empty
+
+type Platform = list<list<Cell>>
+
+let parseFile =
+    File.ReadLines
+    >> Seq.toList
+    >> List.map (Seq.toList >> List.map (fun x ->
+                                         match x with
+                                         | '#' -> Cubed
+                                         | 'O' -> Round
+                                         | _ -> Empty))
+    >> List.transpose // Makes tilting north easier
+
+let calcLoad (platform : Platform) : int =
+    let maxLoad = List.length platform
+    platform
+    |> List.transpose
+    |> List.mapi
+        (fun i line ->
+         (maxLoad - i) * (List.fold (fun acc elem ->
+                                     if elem = Round then 1 + acc else acc) 0 line))
+    |> List.sum
+
+// Only a north title is required if we can also rotate the platform
+let tiltNorth (platform : Platform) : Platform =
+    let nRound n = List.replicate n (Round)
+    let nEmpty n = List.replicate n (Empty)
+    platform
+    |> List.map
+        (fun column ->
+         List.foldBack (fun elem (acc, numRound, numEmpty) ->
+                        match elem with
+                        | Cubed ->
+                            (Cubed :: (nRound numRound) @ (nEmpty numEmpty) @ acc, 0, 0)
+                        | Round -> (acc, numRound + 1, numEmpty)
+                        | Empty -> (acc, numRound, numEmpty + 1)
+                        ) column ([], 0, 0)
+         |> fun (acc, numRound, numEmpty) -> (nRound numRound) @ (nEmpty numEmpty) @ acc)
+
+
+
+
+// Tilts in each direction once
+let cycleOnce (platform : Platform) =
+    let rec rotate acc platform =
+        match platform with
+        | [] -> failwith "Cannot rotate empty platform"
+        | x when List.isEmpty (List.head x) -> acc
+        | x -> // Ensures that we only recurse on a list of nonempty lists
+            rotate' (List.map List.head platform :: acc) (List.map List.tail platform)
+
+    // Tilts then rotates counterclockwise to simulate tilting in NWSE order
+    let rec cycleTilt (n : int) (platform : Platform) =
+        if n > 0 then
+            platform
+            |> tiltNorth
+            |> rotate []
+            |> cycleTilt (n-1)
+        else
+            platform
+
+    cycleTilt 4 platform
+
+let rec cycleN (n : int) (platform : Platform) =
+    if n > 0 then cycleN (n-1) (cycleOnce platform)
+    else platform
+
+// Finds a cycle in the platform states
+let findCycle (platform : Platform) =
+    let rec helper (i : int) (acc : Set<Platform>) (platform : Platform) =
+        if Set.contains platform acc then platform
+        else
+            platform
+            |> cycleOnce
+            |> helper (i+1) (Set.add platform acc)
+
+    helper 0 (Set []) platform
+
+let partOne (platform : Platform) =
+    platform
+    |> tiltNorth
+    |> calcLoad
+
+let partTwo (n : int) (platform : Platform) =
+    let cycleStartPlatform = findCycle platform
+    let rec findDistToStart i platform =
+        if platform = cycleStartPlatform then i
+        else cycleOnce platform |> findDistToStart (i+1)
+
+    let cycleStart = findDistToStart 0 platform
+    let cycleLength = findDistToStart 1 (cycleOnce cycleStartPlatform)
+    let rotations = cycleStart + ((n - cycleStart) % cycleLength)
+
+    platform
+    |> cycleN rotations
+    |> calcLoad
+
+let platformSimple = parseFile "../input_simple.txt"
+let platform = parseFile "../input.txt"
+
+printfn "%A" <| partOne platformSimple
+printfn "%A" <| partOne platform
+
+printfn "%A" <| partTwo 1000000000 platformSimple
+printfn "%A" <| partTwo 1000000000 platform
